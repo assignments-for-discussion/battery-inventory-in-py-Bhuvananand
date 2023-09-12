3baec8fd1987c0efdf0b0a1151f165d50f332fb4
Bhuvananand <135426406+Bhuvananand@users.noreply.github.com>
2023-09-12 13:20:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 
 def count_batteries_by_health(present_capacities):
-  rated_capacity = 120
+  rated_capacity = 120 
   battery_count = {
     "healthy": 0,
     "exchange": 0,
     "failed": 0
   }
+  
   for capacity in present_capacities:
-    SoH = 100 * capacity / rated_capacity
+    SoH = 100 * capacity / rated_capacity #Calculate the SoH % of each battery
 
-    if SoH >= 80:
+    #Increment the values in battery_count based on the given range of SoH
+    if SoH >= 80 and SoH <= 100:
       battery_count["healthy"] += 1
-    elif SoH >=63 and SoH < 80:
+    elif SoH >= 63 and SoH < 80:
       battery_count["exchange"] += 1
-    elif SoH <63:
+    elif SoH < 63 and SoH >= 0:
       battery_count["failed"] += 1
+    else:
+      print('Invalid value of SoH for battery with capacity', capacity) #Error message for SoH values above 100 or below 0
       
   return battery_count
   
