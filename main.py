a7fd12e0a240f668450b9828f5af98f14f47efda
Bhuvananand <135426406+Bhuvananand@users.noreply.github.com>
2023-09-12 13:01:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 
 def count_batteries_by_health(present_capacities):
   rated_capacity = 120
+  battery_count = {}
   for capacity in present_capacities:
     SoH = 100 * capacity / rated_capacity
 
     battery_count = {}
     if SoH >= 80:
-      SoH_dict['healthy'] += 1
+      battery_count['healthy'] += 1
     elif SoH >=63 and SoH < 80:
-      SoH_dict['exchange'] += 1
+      battery_count['exchange'] += 1
     elif SoH <63:
-      SoH_dict['failed'] += 1
+      battery_count['failed'] += 1
       
-  return SoH_dict
+  return battery_count
   return {
     "healthy": 0,
     "exchange": 0,
